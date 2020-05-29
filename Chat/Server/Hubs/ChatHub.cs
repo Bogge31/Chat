9bf3f3ffa65d3ad55c5432d6bc0ec1d845c2175a
Bogge31 <34136797+Bogge31@users.noreply.github.com>
2020-05-29 22:10:12 +0000

    Add project files.

---
+++ Chat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Chat.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendChat(string from, string message)
+        {
+            await Clients.All.SendAsync("ChatSent", new ChatMessage
+            {
+                From = from,
+                Message = message,
+                At = DateTime.Now
+            });
+        }
+    }
+}
---
+++ Chat/Shared/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat.Shared
+{
+    public class ChatMessage
+    {
+        public string From { get; set; }
+        public string Message { get; set; }
+        public DateTime At { get; set; }
+    }
+}
